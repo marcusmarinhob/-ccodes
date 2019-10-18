2989d47c4695a369d55bb40bedbbc78ed7633af1
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2019-10-18 20:42:06 -0300

    Added function definitions but with an empty function body

--- Coursera/Fosdick_Coursera/stats.c
+++ Coursera/Fosdick_Coursera/stats.c
@@ -40,4 +40,43 @@ void main() {
 
 }
 
-/* Add other Implementation File Code Here */
+void print_statistics( unsigned char * data, unsigned int length )
+{
+
+}
+
+
+void print_array( unsigned char * data, unsigned int length )
+{
+
+}
+
+
+int find_median( unsigned char * data, unsigned int length )
+{
+
+}
+
+int find_mean( unsigned char * data, unsigned int length )
+{
+
+}
+
+
+int find_maximum(unsigned char * data, unsigned int length)
+{
+
+}
+
+
+int find_minimum(unsigned char * data, unsigned int length)
+{
+
+}
+
+
+
+void sort_array( unsigned char * data, unsigned int length )
+{
+  
+}
